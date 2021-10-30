40424ce3c7bc4cbd796abb3dd948455f1aa90e74
Joseph Ditton <jditton.atomic@gmail.com>
2021-10-29 19:06:19 -0600

    adds .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
