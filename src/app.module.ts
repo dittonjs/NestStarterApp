e5f684001370d6f6348fd26f97bc26c765deb934
Joseph Ditton <jditton.atomic@gmail.com>
2021-11-07 18:23:01 -0700

    adds foundation for postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(process.env.NODE_ENV === 'development' ? {
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+    } : {
+      url: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: true }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
